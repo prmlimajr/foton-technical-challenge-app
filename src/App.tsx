97c0f7006a27a66cdcd71c0f31eae3bad0117069
Paulo Lima <prmlimajr@hotmail.com>
2021-04-25 03:35:54 -0300

    created bottom tab navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {Text, View, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 import Routes from './routes';
 
 export function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <StatusBar barStyle="dark-content" backgroundColor="#fffcf9" />
       <Routes />
     </NavigationContainer>
   );
--- src/routes/Tab.routes.tsx
+++ src/routes/Tab.routes.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
+
 import StackRoutes from './Stack.routes';
 import AddBooks from '../pages/AddBooks';
 import Profile from '../pages/Profile';
@@ -8,10 +10,55 @@ const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={StackRoutes} />
-      <Tab.Screen name="AddBooks" component={AddBooks} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: '#000',
+        style: {
+          height: 60,
+          paddingBottom: 10,
+          paddingTop: 10,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={StackRoutes}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="home" color="#000" size={24} />
+            ) : (
+              <Icon name="home" color="#BFBEBf" size={24} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="AddBooks"
+        component={AddBooks}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="plus" color="#000" size={24} />
+            ) : (
+              <Icon name="plus" color="#BFBEBf" size={24} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="user" color="#000" size={24} />
+            ) : (
+              <Icon name="user" color="#BFBEBf" size={24} />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
